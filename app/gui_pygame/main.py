5e817372afb0f19c9adb3ea17a2e5ebb17a41dd1
leo <basov.leo@googlemail.com>
2019-07-09 10:51:35 +0200

    Added reset function.

--- app/gui_pygame/main.py
+++ app/gui_pygame/main.py
@@ -16,6 +16,7 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
 
 def main():
 	#initiize pygame
@@ -47,7 +48,7 @@ def main():
 
 		for creature in wrld.creatures:
 			point = (25 + 40*creature.position[0], 25 + 40*creature.position[1], 25 + 40*creature.position[2])
-			_draw_point(DISPLAYSURF, point, BLUE)
+			_draw_point(DISPLAYSURF, point, BLUE, int(max(1, creature.size*20)))
 
 		for food in wrld.food:
 			point = (25 + 40*food.position[0], 25 + 40*food.position[1], 25 + 40*food.position[2])
@@ -60,6 +61,8 @@ def main():
 			elif event.type == KEYDOWN:
 				if event.key == K_SPACE:
 					RUN = not RUN
+				elif event.key == K_r:
+					reset(wrld, creature_number, creature_sense, creature_speed, creature_size, food_number)
 
 		pygame.display.update()
 		fps_clock.tick(FPS)
@@ -70,11 +73,16 @@ def main():
 			if wrld.time == 0 and len(wrld.creatures) > 0:
 				wrld.generate_food(food_number)
 
+def reset(wrld, creature_number, creature_sense, creature_speed, creature_size, food_number):
+	wrld.generate_creatures(creature_number, creature_sense, creature_speed, creature_size)
+	wrld.generate_food(food_number)
+
 def draw_frame(display_surf):
-	pygame.draw.rect(display_surf, (0, 0, 0), (25, 25, 850 - 50, 850 - 50), 5)
+	pygame.draw.rect(display_surf, (0, 0, 0), (20, 20, 850 - 40, 850 - 40), 5)
 
 def _draw_point(surface_surf, point, color, size = 5):
 		pygame.draw.circle(surface_surf, color, (int(point[0]), int(point[1])), size, 0)
+		pygame.draw.circle(surface_surf, BLACK, (int(point[0]), int(point[1])), size, min(size, 1))
 
 if __name__ == "__main__":
 	main()
