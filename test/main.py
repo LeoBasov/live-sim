d9dc3508bd678b140c4ade7d6aee5f393e08c02a
Leo Basov <basov_leo@web.de>
2019-07-20 17:43:59 +0200

    Extended smart creature.

--- test/main.py
+++ test/main.py
@@ -2,5 +2,7 @@
 
 import unittest
 
+from smart_creature_test import*
+
 if __name__ == '__main__':
 	unittest.main()
---
+++ test/smart_creature_test.py
@@ -0,0 +1,10 @@
+import unittest
+import sys
+
+sys.path.append('../.')
+
+import live_sim.smart_creature as sc
+
+class UtilityTest(unittest.TestCase):
+	def test_brain(self):
+		brain = sc.Brain()
